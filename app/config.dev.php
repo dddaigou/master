2dca5865922e54abc498c9cfa9054b30fb0d37ec
Jeff Peng <Jeff Peng>
2016-04-03 00:13:11 +0800

    修改配置

--- app/config.dev.php
+++ app/config.dev.php
@@ -1,22 +1,22 @@
 <?php
 return [
-    'url_domain_root'   => '.dev.test.com',
+    'url_domain_root'   => '.dev.dddaigou.net',
     // DATABASE
     'database'      => [
         'type'      => 'mysql',         // 数据库类型
         'hostname'  => '127.0.0.1',  // 服务器地址
-        'database'  => 'hk8591',        // 数据库名
+        'database'  => 'dddaigou',        // 数据库名
         'username'  => 'test',          // 数据库用户名
         'password'  => 'aaaaaa', // 数据库密码
         'hostport'  => '3306',          // 数据库连接端口
         'charset'   => 'utf8',          // 数据库编码默认采用utf8
-        'prefix'    => 't8_',           // 数据库表前缀
+        'prefix'    => 'dd_',           // 数据库表前缀
         'debug'     => true,            // 调试开关
     ],
     // SESSION
     'session'       => [
         'auto_start'=> true,
-        'domain'    => '.dev.test.com',
+        'domain'    => '.dev.dddaigou.net',
         'type'      => 'redis',
         'host'      => '127.0.0.1',
         'port'      => 6379,
@@ -31,6 +31,6 @@ return [
     // api
     'api'           => [
         'url'       => 'http://127.0.0.1/',
-        'host'      => 'api.dev.test.com'
+        'host'      => 'api.dev.dddaigou.net'
     ],
 ];
